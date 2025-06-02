bd4fa15a62b28418d616814f4e225a7c56b67a9a
chengyang1380 <18714826+chengyang1380@users.noreply.github.com>
2025-06-02 12:38:57 +0800

    Updated SampleTextFeature and tests.

--- SwiftUI_TCA_Practice/SampleTextFeature.swift
+++ SwiftUI_TCA_Practice/SampleTextFeature.swift
@@ -26,6 +26,7 @@ struct SampleTextFeature {
 
     struct Environment {
         var loadText: () -> Effect<Result<String, URLError>>
+        var queue: AnySchedulerOf<DispatchQueue>
     }
 
     var body: some ReducerOf<Self> {
@@ -33,8 +34,13 @@ struct SampleTextFeature {
             switch action {
             case .load:
                 state.loading = true
-                return environment.loadText()
-                    .map { .loaded($0) }
+                return .run { send in
+                    try await environment.queue.sleep(for: .zero)
+                }.concatenate(
+                    with:
+                        environment.loadText()
+                        .map { .loaded($0) }
+                )
             case .loaded(let result):
                 state.loading = false
                 do {
@@ -49,12 +55,25 @@ struct SampleTextFeature {
 }
 
 extension SampleTextFeature.Environment: DependencyKey {
+    static var sampleRequest: Effect<Result<String, URLError>> {
+        .run { send in
+            let request = URLRequest(
+                url: URL(string: "https://example.com/sample-text")!
+            )
+            let (data, _) = try await URLSession.shared.data(for: request)
+            let result = String(data: data, encoding: .utf8) ?? ""
+            await send(.success(result))
+        }
+    }
+
     static var liveValue = Self(
-        loadText: { .run { send in await send(.success("Hello, World!")) } }  // throw some error }}
+        loadText: { sampleRequest },
+        queue: .global()
     )
 
     static var testValue = Self(
-        loadText: { .run { send in await send(.success("Hello, World!")) } }  // send(.failure(URLError.init(.badURL))) }}
+        loadText: { .run { send in await send(.success("Hello, World!")) } },  // send(.failure(URLError.init(.badURL))) }}
+        queue: .global()
     )
 }
 
--- SwiftUI_TCA_PracticeTests/SampleTextFeatureTests.swift
+++ SwiftUI_TCA_PracticeTests/SampleTextFeatureTests.swift
@@ -6,6 +6,7 @@
 //
 
 import Testing
+import Foundation
 import ComposableArchitecture
 
 @testable import SwiftUI_TCA_Practice
@@ -13,15 +14,26 @@ import ComposableArchitecture
 @MainActor
 struct SampleTextFeatureTests {
 
+    let scheduler = DispatchQueue.test
+
     @Test
     func sampleTextRequest() async throws {
         let store = TestStore(initialState: SampleTextFeature.State()) {
             SampleTextFeature()
+        } withDependencies: {
+            $0.sampleTextFeatureEnvironment.loadText = {
+                return .run { send in
+                    await send(.success("Hello, World!"))
+                }
+            }
+            $0.sampleTextFeatureEnvironment.queue = scheduler.eraseToAnyScheduler() // .immediate
         }
-        
+
         await store.send(.load) {
             $0.loading = true
         }
+
+        await scheduler.advance() // 替換 .immediate 即可刪除
 
         await store.receive(.loaded(.success("Hello, World!"))) {
             $0.loading = false
